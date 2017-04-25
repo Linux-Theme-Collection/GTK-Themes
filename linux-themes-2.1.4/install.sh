1334a6186fbd68e8804f1b951d409beacbcbf3ef
yangxins <yangmame@icloud.com>
2017-04-25 15:55:02 +0800

    fix

--- linux-themes-2.1.4/install.sh
+++ linux-themes-2.1.4/install.sh
@@ -20,9 +20,9 @@ echo "复制字体..."
 sudo cp -rf fonts/ /usr/share/
 
 echo "复制grub主题"
-if  (cat /etc/* | grep yum)
+if  (cat /etc/* | grep yum > /dev/null 2>&1)
 then sudo mkdir /boot/grub2/themes/&&sudo cp -rf grub-theme/ /boot/grub2/themes/
-elif (cat /etc/* | grep apt)
+elif (cat /etc/* | grep apt > /dev/null 2>&1)
 then mkdir backup&&sudo mkdir /boot/grub/themes/&&sudo cp -rf grub-theme/ /boot/grub/themes/
 mv /root/.bashrc $DIR/backup/root-bashrc&&mv /home/$USER/.bashrc $DIR/backup/$USER-bashrc
 cp bashrc /root/.bashrc&&cp bashrc /home/$USER/.bashrc&&chmod 777 /home/$USER/.bashrc

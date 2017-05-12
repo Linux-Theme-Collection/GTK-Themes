89ae3cb0e5176f6e68ba08f901694fbd55c8ff3a
yangxins <yangmame@icloud.com>
2017-05-12 18:02:35 +0800

    up

--- linux-themes-2.1.4/install.sh
+++ linux-themes-2.1.4/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if (ls /etc/ | grep yum > /dev/null 2>&1)
+then PM=yum
+elif (ls /etc/ | grep apt > /dev/null 2>&1)
+then PM=apt
+else PM=else
+fi
+
 DIR=$(pwd)
 USER=$(ls /home/)
 
@@ -20,9 +27,9 @@ echo "复制字体..."
 sudo cp -rf fonts/ /usr/share/
 
 echo "复制grub主题"
-if  (nohup cat /etc/* | grep yum > /dev/null 2>&1)
+if  (cat /etc/* | grep yum > /dev/null 2>&1)
 then sudo mkdir /boot/grub2/themes/&&sudo cp -rf grub-theme/ /boot/grub2/themes/
-elif (nohup cat /etc/* | grep apt > /dev/null 2>&1)
+elif (cat /etc/* | grep apt > /dev/null 2>&1)
 then mkdir backup&&sudo mkdir /boot/grub/themes/&&sudo cp -rf grub-theme/ /boot/grub/themes/
 mv /root/.bashrc $DIR/backup/root-bashrc&&mv /home/$USER/.bashrc $DIR/backup/$USER-bashrc
 cp bashrc /root/.bashrc&&cp bashrc /home/$USER/.bashrc&&chmod 777 /home/$USER/.bashrc

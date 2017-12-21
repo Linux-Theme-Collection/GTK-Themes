aeada66305ed9caf695b7e8caec40c6170e2ba49
YangMame <yangmame@icloud.com>
2017-12-21 12:00:39 +0800

    清理

--- install-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'copying  all......'
-sudo cp -rf icons/*  /usr/share/icons/
-sudo cp -rf themes/*  /usr/share/themes/
-sudo cp -rf backgrounds/*  /usr/share/backgrounds/
-echo 'done.'
